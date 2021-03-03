eff1db7bfe4889f873f72837ac56701f88e2d57b
BrahderLau <khlau_1012@hotmail.com>
2021-03-03 10:49:17 +0800

    Fix Layout Alignment

--- Malaysia MCPL/src/components/Footers/FooterAdmin.js
+++ Malaysia MCPL/src/components/Footers/FooterAdmin.js
@@ -10,7 +10,7 @@ export default function FooterAdmin() {
           <div className="flex flex-wrap items-center md:justify-between justify-center ">
             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
               <div className="text-sm text-gray-600 font-semibold py-1">
-              <a className="text-gray-600 text-sm font-semibold py-1">
+                <a className="text-gray-600 text-sm font-semibold py-1">
                   Organized by Magic Chess Union (MCU)
                 </a><br/>
                 <a className="text-gray-600 text-sm font-semibold py-1">
@@ -20,9 +20,8 @@ export default function FooterAdmin() {
                   href="https://brahderlau.netlify.app/"
                   className="text-gray-600 hover:text-gray-800 text-sm font-semibold py-1"
                 >
-                  Copyright © {new Date().getFullYear()}{" "} BrahderLau
+                  Copyright © {new Date().getFullYear()}{" "} BrahderLau.
                 </a>
-                .
               </div>
             </div>
           </div>
